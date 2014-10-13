5aa4e3a83a4eec4308d6799c72d74bde5f544ab8
thomaswp <thomaswprice@msn.com>
2014-10-13 19:27:54 -0400

    Deleted extraneous files

--- dl/loopinvariant.js
+++
@@ -1,5 +0,0 @@
-function gracecode_loopinvariant() {
-  return this;
-}
-if (gctCache)
-  gctCache['loopinvariant'] = "path:\n loopinvariant\nclasses:\npublic:\nconfidential:\nfresh-methods:\nmodules:\n";
